0c884e3513ab9300fd4510fda3af7f221db44b07
Gabriel de Souza <alvesgabrieldesouza@gmail.com>
2021-04-20 23:14:26 -0300

    tentativa de implementar a api

--- consumo-api/js/graphs.js
+++ consumo-api/js/graphs.js
@@ -1,3 +1,32 @@
+var url = "https://www.cheapshark.com/api/1.0/games?id=612"
+fetch(url)
+    .then(response => response.json())
+
+    .then(json => {
+        var games = json.deals;
+        console.log(games)
+    })    
+
+geraGraphCard()
+
+function geraGraphCard(){
+    var container = document.getElementById(`gridGraphs`)
+    container.innerHTML = ""
+    container.innerHTML = `<div class="row row-cols-1 row-cols-md-3 g-3">
+    <div class="col">
+        <div class="card">
+            <canvas class="line-chart" id="myChart1"></canvas>
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+    </div>`
+    
+}
+
+
 var ctx = document.getElementById('myChart1').getContext('2d');
         var myChart = new Chart(ctx, {
             type: 'line',
@@ -71,6 +100,42 @@ var ctx = document.getElementById('myChart2').getContext('2d');
 var ctx = document.getElementById('myChart3').getContext('2d');
         var myChart = new Chart(ctx, {
             type: 'doughnut',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+        
+var ctx = document.getElementById('myChart4').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'radar',
             data: {
                 labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
                 datasets: [{
