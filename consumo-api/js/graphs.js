40d162f645c6c1ddc1f4817592bf999b7b54ccac
Gabriel de Souza <alvesgabrieldesouza@gmail.com>
2021-04-23 23:25:48 -0300

    função de pesquisa incompleto

--- consumo-api/js/graphs.js
+++ consumo-api/js/graphs.js
@@ -1,24 +1,26 @@
-var url = "https://www.cheapshark.com/api/1.0/games?id=40"
-
-
-fetch(url)
+function pesquisaJogo(){
+    let gamename = document.getElementById("searchGame").value
+    var url = `https://www.cheapshark.com/api/1.0/deals?title=${gamename}`
+    
+    console.log(url)
+    fetch(url)
 
     .then(response => response.json())
 
     .then(json => {
-        let namejson = json.info
-        var games = json.deals;
+        let namejson = json[0].title
+        var games = json;
         console.log(games);
         
 
-        let ctx = document.getElementById('myChart1').getContext('2d');
-        let myChart = new Chart(ctx, {
+        var ctx = document.getElementById('myChart1').getContext('2d');
+        var myChart1 = new Chart(ctx, {
             type: 'line',
             data: {
                 labels: ['loja 1', 'loja 2', 'loja 3', 'loja 4', 'loja 5',],
                 datasets: [{
-                    label: namejson.title,
-                    data: [games[0].price, games[1].price, games[2].price, games[3].price, games[4].price],
+                    label: namejson,
+                    data: [games[0].dealRating, games[1].dealRating, games[2].dealRating, games[3].dealRating, games[4].dealRating],
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
@@ -53,8 +55,8 @@ fetch(url)
             data: {
                 labels: ['loja 1', 'loja 2', 'loja 3', 'loja 4', 'loja 5',],
                 datasets: [{
-                    label: namejson.title,
-                    data: [games[0].price, games[1].price, games[2].price, games[3].price, games[4].price],
+                    label: namejson,
+                    data: [games[0].dealRating, games[1].dealRating, games[2].dealRating, games[3].dealRating, games[4].dealRating],
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
@@ -89,8 +91,8 @@ fetch(url)
             data: {
                 labels: ['loja 1', 'loja 2', 'loja 3', 'loja 4', 'loja 5',],
                 datasets: [{
-                    label: namejson.title,
-                    data: [games[0].price, games[1].price, games[2].price, games[3].price, games[4].price],
+                    label: namejson,
+                    data: [games[0].dealRating, games[1].dealRating, games[2].dealRating, games[3].dealRating, games[4].dealRating],
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
@@ -125,8 +127,8 @@ fetch(url)
             data: {
                 labels: ['loja 1', 'loja 2', 'loja 3', 'loja 4', 'loja 5',],
                 datasets: [{
-                    label: namejson.title,
-                    data: [games[0].price, games[1].price, games[2].price, games[3].price, games[4].price],
+                    label: namejson,
+                    data: [games[0].dealRating, games[1].dealRating, games[2].dealRating, games[3].dealRating, games[4].dealRating],
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
@@ -155,13 +157,17 @@ fetch(url)
             }
 
         });
-         
-
     })
+ 
+}
+
 
 var ids = ["myChart1", "myChart2", "myChart3", "myChart4"]
 var classes = ["line-chart", "bar-chart", "radar-chart", "doughnut-chart"]
+
 function geraGraphCard() {
+    
+    
     var container = document.getElementById(`gridGraphs`)
     container.innerHTML = ""
     for (let i = 0; i < ids.length; i++) {
@@ -187,74 +193,3 @@ geraGraphCard()
 
 
 
-// var ctx = document.getElementById('myChart3').getContext('2d');
-// var myChart = new Chart(ctx, {
-//     type: 'doughnut',
-//     data: {
-//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//         datasets: [{
-//             label: '# of Votes',
-//             data: [12, 19, 3, 5, 2, 3],
-//             backgroundColor: [
-//                 'rgba(255, 99, 132, 0.2)',
-//                 'rgba(54, 162, 235, 0.2)',
-//                 'rgba(255, 206, 86, 0.2)',
-//                 'rgba(75, 192, 192, 0.2)',
-//                 'rgba(153, 102, 255, 0.2)',
-//                 'rgba(255, 159, 64, 0.2)'
-//             ],
-//             borderColor: [
-//                 'rgba(255, 99, 132, 1)',
-//                 'rgba(54, 162, 235, 1)',
-//                 'rgba(255, 206, 86, 1)',
-//                 'rgba(75, 192, 192, 1)',
-//                 'rgba(153, 102, 255, 1)',
-//                 'rgba(255, 159, 64, 1)'
-//             ],
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             y: {
-//                 beginAtZero: true
-//             }
-//         }
-//     }
-// });
-
-// var ctx = document.getElementById('myChart4').getContext('2d');
-// var myChart = new Chart(ctx, {
-//     type: 'radar',
-//     data: {
-//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//         datasets: [{
-//             label: '# of Votes',
-//             data: [12, 19, 3, 5, 2, 3],
-//             backgroundColor: [
-//                 'rgba(255, 99, 132, 0.2)',
-//                 'rgba(54, 162, 235, 0.2)',
-//                 'rgba(255, 206, 86, 0.2)',
-//                 'rgba(75, 192, 192, 0.2)',
-//                 'rgba(153, 102, 255, 0.2)',
-//                 'rgba(255, 159, 64, 0.2)'
-//             ],
-//             borderColor: [
-//                 'rgba(255, 99, 132, 1)',
-//                 'rgba(54, 162, 235, 1)',
-//                 'rgba(255, 206, 86, 1)',
-//                 'rgba(75, 192, 192, 1)',
-//                 'rgba(153, 102, 255, 1)',
-//                 'rgba(255, 159, 64, 1)'
-//             ],
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             y: {
-//                 beginAtZero: true
-//             }
-//         }
-//     }
-// });
