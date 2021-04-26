b5493338870250a2aa831c960ac37048b72ef70d
Anderpxl-cmd <76409491+Anderpxl-cmd@users.noreply.github.com>
2021-04-26 10:45:13 -0300

    inserção de conteúdo na home do index.html, colocado o funcionamento do botão enter para efetuar a pesquisa e estilização do site

--- consumo-api/js/graphs.js
+++ consumo-api/js/graphs.js
@@ -1,10 +1,20 @@
+var input = document.getElementById("searchGame")
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("searchButton").click()
+  }
+})
+
 function pesquisaJogo() {
 
     let gameName = document.getElementById("searchGame").value
 
     var url = `https://www.cheapshark.com/api/1.0/deals?title=${gameName}`
 
-    var classes = ["de linha", "de barra", "de radar", "de donut", "polar"]
+    var classes = ["de-linha", "de-barra", "de-radar", "de-donut", "polar"]
+    var id = ["myChart1", "myChart2", "myChart3", "myChart4", "myChart5"]
+    var types = ["radar", "doughnut", "polarArea", "bar", "line"]
 
     console.log(url)
     fetch(url)
@@ -12,8 +22,13 @@ function pesquisaJogo() {
         .then(response => response.json())
 
         .then(json => {
-            var nameJSON = json[0].title
+            
             var games = json;
+            if (games.length === 0) {
+                alert("Jogo não encontrado")
+            }
+
+            var nameJSON = json[0].title
             gameName = nameJSON
             console.log(games)
             console.log(gameName)
@@ -25,204 +40,141 @@ function pesquisaJogo() {
             function geraGraphCard() {
 
                 var nomeJogo = gameName
-                var id = ["myChart1", "myChart2", "myChart3", "myChart4", "myChart5"]
                 var container = document.getElementById(`gridGraphs`)
                 container.innerHTML = ""
                 for (let i = 0; i < id.length; i++) {
                     container.innerHTML +=
-                        `
-                <div class="col">
-                    <div class="card">
-                        <canvas class="${classes[i]}" id=${id[i]}></canvas>
+                    `
+                    <div class="col">
+                        <div class="card">
+                            <canvas class="${classes[i]}" id=${id[i]}></canvas>
                         <div class="card-body">
                             <h5 class="card-title">${nomeJogo}</h5>
                             <p class="card-text">Nesse gráfico ${classes[i]} é mostrado a diferença de preços do jogo ${nomeJogo} em diferentes lojas.</p>
                         </div>
                     </div>
-                `
+                    `
                 }
             }
 
             geraGraphCard()
 
-            var ctx = document.getElementById('myChart1').getContext('2d');
-            var myChart = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: ['loja 1', 'loja 2', 'loja 3', 'loja 4', 'loja 5'],
-                    datasets: [{
-                        label: nameJSON,
-                        data: [games[0].normalPrice, games[1].normalPrice, games[2].normalPrice, games[3].normalPrice, games[4].normalPrice],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
-                    }
-                }
+            for (let i = 0; i < classes.length; i++) {
 
-            });
-            ctx = document.getElementById('myChart2').getContext('2d');
-            myChart = new Chart(ctx, {
-                type: 'bar',
-                data: {
-                    labels: ['loja 1', 'loja 2', 'loja 3', 'loja 4', 'loja 5'],
-                    datasets: [{
-                        label: nameJSON,
-                        data: [games[0].normalPrice, games[1].normalPrice, games[2].normalPrice, games[3].normalPrice, games[4].normalPrice],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true
+                if (games.length == 4) {
+                    var ctx = document.getElementById(id[i]).getContext('2d');
+                    var myChart = new Chart(ctx, {
+                        type: types[i],
+                        data: {
+                            labels: ['loja 1', 'loja 2', 'loja 3', 'loja 4',],
+                            datasets: [{
+                                label: nameJSON,
+                                data: [games[0].normalPrice, games[1].normalPrice, games[2].normalPrice, games[3].normalPrice],
+                                backgroundColor: [
+                                    'rgba(255, 99, 132, 0.2)',
+                                    'rgba(54, 162, 235, 0.2)',
+                                    'rgba(255, 206, 86, 0.2)',
+                                    'rgba(75, 192, 192, 0.2)',
+                                    'rgba(153, 102, 255, 0.2)',
+                                    'rgba(255, 159, 64, 0.2)'
+                                ],
+                                borderColor: [
+                                    'rgba(255, 99, 132, 1)',
+                                    'rgba(54, 162, 235, 1)',
+                                    'rgba(255, 206, 86, 1)',
+                                    'rgba(75, 192, 192, 1)',
+                                    'rgba(153, 102, 255, 1)',
+                                    'rgba(255, 159, 64, 1)'
+                                ],
+                                borderWidth: 1
+                            }]
+                        },
+                        options: {
+                            scales: {
+                                y: {
+                                    beginAtZero: true
+                                }
+                            }
                         }
-                    }
-                }
 
-            });
-            ctx = document.getElementById('myChart3').getContext('2d');
-            myChart = new Chart(ctx, {
-                type: 'radar',
-                data: {
-                    labels: ['loja 1', 'loja 2', 'loja 3', 'loja 4', 'loja 5'],
-                    datasets: [{
-                        label: nameJSON,
-                        data: [games[0].normalPrice, games[1].normalPrice, games[2].normalPrice, games[3].normalPrice, games[4].normalPrice],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
-                    }
+                    });
                 }
+                else if (games.length == 3) {
+                    var ctx = document.getElementById(id[i]).getContext('2d');
+                    var myChart = new Chart(ctx, {
+                        type: types[i],
+                        data: {
+                            labels: ['loja 1', 'loja 2', 'loja 3',],
+                            datasets: [{
+                                label: nameJSON,
+                                data: [games[0].normalPrice, games[1].normalPrice, games[2].normalPrice],
+                                backgroundColor: [
+                                    'rgba(255, 99, 132, 0.2)',
+                                    'rgba(54, 162, 235, 0.2)',
+                                    'rgba(255, 206, 86, 0.2)',
+                                    'rgba(75, 192, 192, 0.2)',
+                                    'rgba(153, 102, 255, 0.2)',
+                                    'rgba(255, 159, 64, 0.2)'
+                                ],
+                                borderColor: [
+                                    'rgba(255, 99, 132, 1)',
+                                    'rgba(54, 162, 235, 1)',
+                                    'rgba(255, 206, 86, 1)',
+                                    'rgba(75, 192, 192, 1)',
+                                    'rgba(153, 102, 255, 1)',
+                                    'rgba(255, 159, 64, 1)'
+                                ],
+                                borderWidth: 1
+                            }]
+                        },
+                        options: {
+                            scales: {
+                                y: {
+                                    beginAtZero: true
+                                }
+                            }
+                        }
 
-            });
-            ctx = document.getElementById('myChart4').getContext('2d');
-            myChart = new Chart(ctx, {
-                type: 'doughnut',
-                data: {
-                    labels: ['loja 1', 'loja 2', 'loja 3', 'loja 4', 'loja 5'],
-                    datasets: [{
-                        label: nameJSON,
-                        data: [games[0].normalPrice, games[1].normalPrice, games[2].normalPrice, games[3].normalPrice, games[4].normalPrice],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
-                    }
+                    });
                 }
-            });
-            ctx = document.getElementById('myChart5').getContext('2d');
-            myChart = new Chart(ctx, {
-                type: 'polarArea',
-                data: {
-                    labels: ['loja 1', 'loja 2', 'loja 3', 'loja 4', 'loja 5'],
-                    datasets: [{
-                        label: nameJSON,
-                        data: [games[0].normalPrice, games[1].normalPrice, games[2].normalPrice, games[3].normalPrice, games[4].normalPrice],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true
+                else {
+                    var ctx = document.getElementById(id[i]).getContext('2d');
+                    var myChart = new Chart(ctx, {
+                        type: types[i],
+                        data: {
+                            labels: ['loja 1', 'loja 2', 'loja 3', 'loja 4', 'loja 5'],
+                            datasets: [{
+                                label: nameJSON,
+                                data: [games[0].normalPrice, games[1].normalPrice, games[2].normalPrice, games[3].normalPrice, games[4].normalPrice],
+                                backgroundColor: [
+                                    'rgba(255, 99, 132, 0.2)',
+                                    'rgba(54, 162, 235, 0.2)',
+                                    'rgba(255, 206, 86, 0.2)',
+                                    'rgba(75, 192, 192, 0.2)',
+                                    'rgba(153, 102, 255, 0.2)',
+                                    'rgba(255, 159, 64, 0.2)'
+                                ],
+                                borderColor: [
+                                    'rgba(255, 99, 132, 1)',
+                                    'rgba(54, 162, 235, 1)',
+                                    'rgba(255, 206, 86, 1)',
+                                    'rgba(75, 192, 192, 1)',
+                                    'rgba(153, 102, 255, 1)',
+                                    'rgba(255, 159, 64, 1)'
+                                ],
+                                borderWidth: 1
+                            }]
+                        },
+                        options: {
+                            scales: {
+                                y: {
+                                    beginAtZero: true
+                                }
+                            }
                         }
-                    }
-                }
 
-            });
+                    });
+                }
+            }
         })
 }
